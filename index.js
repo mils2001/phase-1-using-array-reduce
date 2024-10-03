adc750c0fcdbc05ecfb275a06ed069959c4320cd
chris <Mileschris466@gmail.com>
2024-10-04 02:53:29 +0300

    reduce

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteryData = [
+    { id: 1, assembled: 10 },
+    { id: 2, assembled: 15 },
+    { id: 3, assembled: 6 },
+  ];
+  
+  
+  const batteryReducer = (data) => {
+    return data.reduce((total, battery) => total + battery.assembled, 0);
+  };
+  
+  
+  const totalBatteries = batteryReducer(batteryData);
+  
+  module.exports = { totalBatteries, batteryReducer };
+  
+
+
